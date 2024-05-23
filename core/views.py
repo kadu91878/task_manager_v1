20bd9847229f9ae55710350419c5b0844125add1
kadu91878 <kadu_91878@hotmail.com>
2024-05-23 20:32:30 -0300

    feat: views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from .models import User, Project, Task, Tag
+from .serializers import (
+    UserSerializer,
+    ProjectSerializer,
+    TaskSerializer,
+    TagSerializer,
+)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+    def partial_update(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.creator != request.user:
+            raise PermissionDenied("Apenas o criador do projeto pode adicionar ou remover membros.")
+        return super().partial_update(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        obj = self.get_object()
+        if obj.creator != request.user:
+            raise PermissionDenied("Você não tem permissão para excluir este projeto.")
+        return super().destroy(request, *args, **kwargs)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        project = serializer.validated_data["project"]
+        if not project.members.filter(id=self.request.user.id).exists() and self.request.user.id != project.creator_id:
+            raise PermissionDenied(
+                "Você não tem permissão para criar tarefas neste projeto."
+            )
+        serializer.save(project=project)
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.status == "COMPLETED":
+            raise PermissionDenied("Tarefas concluídas não podem ser editadas.")
+        return super().update(request, *args, **kwargs)
+
+
+class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.IsAuthenticated]
